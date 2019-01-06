ec295ecd006f9057d9f02347f171a610f4f3473c
F-Kapok <5219824@qq.com>
2019-01-06 21:11:53 +0800

    update

---
+++ buyer/src/main/java/com/fans/controller/PayController.java
@@ -0,0 +1,14 @@
+package com.fans.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName PayController
+ * @Description:
+ * @Author fan
+ * @Date 2019-01-06 14:53
+ * @Version 1.0
+ **/
+@Controller
+public class PayController {
+}
