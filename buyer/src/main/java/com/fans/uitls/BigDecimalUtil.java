fd6c9c6095b04d669bcb7b709d2d50b617d2e966
F-Kapok <5219824@qq.com>
2019-01-11 01:48:53 +0800

    weChat Pay

---
+++ buyer/src/main/java/com/fans/uitls/BigDecimalUtil.java
@@ -0,0 +1,11 @@
+package com.fans.uitls;
+
+/**
+ * @ClassName BigDecimalUtil
+ * @Description: TODO
+ * @Author fan
+ * @Date 2019-01-11 00:36
+ * @Version 1.0
+ **/
+public class BigDecimalUtil {
+}
