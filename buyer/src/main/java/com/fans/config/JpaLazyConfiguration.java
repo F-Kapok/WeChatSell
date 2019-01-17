2017394a980bb38981016a9f98fa8e5a489aaaa0
F-Kapok <5219824@qq.com>
2019-01-17 09:47:30 +0800

    update

--- buyer/src/main/java/com/fans/config/JpaLazyConfiguration.java
+++ buyer/src/main/java/com/fans/config/JpaLazyConfiguration.java
@@ -1,11 +1,22 @@
 package com.fans.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+
 /**
  * @ClassName JpaLazyConfiguration
- * @Description: TODO
+ * @Description:
  * @Author fan
  * @Date 2019-01-17 09:41
  * @Version 1.0
  **/
+@Configuration
 public class JpaLazyConfiguration {
+
+    @Bean
+    public OpenEntityManagerInViewFilter openEntityManagerInViewFilter() {
+        return new OpenEntityManagerInViewFilter();
+    }
+    
 }
