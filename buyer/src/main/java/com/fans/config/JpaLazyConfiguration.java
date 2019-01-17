6571ea05e99c6a3b0887cee31e7e3a99000bda65
F-Kapok <5219824@qq.com>
2019-01-17 09:46:20 +0800

    update

---
+++ buyer/src/main/java/com/fans/config/JpaLazyConfiguration.java
@@ -0,0 +1,11 @@
+package com.fans.config;
+
+/**
+ * @ClassName JpaLazyConfiguration
+ * @Description: TODO
+ * @Author fan
+ * @Date 2019-01-17 09:41
+ * @Version 1.0
+ **/
+public class JpaLazyConfiguration {
+}
